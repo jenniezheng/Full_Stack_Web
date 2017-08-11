93ac70aee7565d0fc6b60d69bc5a0246abd9689e
Jennie <jenniezheng321@gmail.com>
2017-08-10 22:33:28 -0700

    make commit

--- jquery_practice/jquery_practice.js
+++
@@ -1,7 +0,0 @@
-$("div").css({ "background-color":"purple" });
-
-
-$("div.highlight").css({ "width":"200px" });
-$("#third").css({ "border":"orange 5px solid" });
-
-$("div:first-child").css({ "color":"pink" });
