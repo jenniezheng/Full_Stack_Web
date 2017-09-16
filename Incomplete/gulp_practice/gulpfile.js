4e5f5a203d67d54db2178be0ccb20877944d8f80
Jennie Zheng <jenniezheng321@gmail.com>
2017-09-16 14:05:22 -0700

    updating

---
+++ Incomplete/gulp_practice/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+ // Concatenate JS Files
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('build/js'));
+});
+ // Default Task
+gulp.task('default', ['scripts']);
